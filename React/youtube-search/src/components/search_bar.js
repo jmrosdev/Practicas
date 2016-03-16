25ea73b17da807ebe88b59ba1de5713d8475c716
Juan Manuel Ros <juanmanuelros@iMac-de-Juan.local>
2016-03-16 23:02:14 +0100

    Hasta el capitulo 18. -Como sacar el dato que introducimos por un input en un p (state)

--- React/youtube-search/src/components/search_bar.js
+++ React/youtube-search/src/components/search_bar.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
-const SearchBar = () => {
-	return <input />;
-};
+class SearchBar extends React.Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = { term: ''  };
+
+	}
+	render(){
+		return (
+		<div>
+			<input onChange={event => this.setState({term: event.target.value})} />
+			<p>valor del input {this.state.term}</p>
+		</div>
+		);
+	}
+
+	onInputChange(event) {
+		console.log(event.target.value);
+	}
+}
 
 export default SearchBar;
--- React/youtube-search/webpack.config.js
+++ React/youtube-search/webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
       exclude: /node_modules/,
       loader: 'babel'
     }]
-  }
+  },
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
